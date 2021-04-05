27e38b710926ab5619f496bce56db69b7c2a5264
dcereijodo <dcereijo@digitalorigin.com>
2021-04-05 15:56:38 +0200

    enable caching

--- tfmkt/settings.py
+++ tfmkt/settings.py
@@ -18,7 +18,15 @@ FEED_URI = 'stdout:'
 EXTENSIONS = {
    'scrapy.extensions.closespider.CloseSpider': 500
 }
+DOWNLOADER_MIDDLEWARES = {
+   'scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware': 500
+}
 
 CLOSESPIDER_PAGECOUNT = 0
 
 LOG_LEVEL = 'ERROR'
+
+# HttpCacheMiddleware settings
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+HTTPCACHE_ENABLED = True
+HTTPCACHE_DIR = 'httpcache'
