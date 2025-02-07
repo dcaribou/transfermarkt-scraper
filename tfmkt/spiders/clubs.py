5c9366fc08e17368f5f29ba9d8284955a934f428
Bruno Martins de Sá Freire <bruno290103@gmail.com>
2025-02-07 09:37:49 -0300

    .

--- tfmkt/spiders/clubs.py
+++ tfmkt/spiders/clubs.py
@@ -7,9 +7,8 @@ class ClubsSpider(BaseSpider):
 
     def parse(self, response, parent):
         """
-        Parse the competition page. Transfermarkt pages may contain more than one
-        "responsive-table" block for teams (for example, one for first tier and another for second tier).
-        We iterate over all tables that appear to have team data.
+        Parse competition page. From this page we collect all competition's
+        team URLs from all 'responsive-table' blocks that appear to contain club info.
         
         @url https://www.transfermarkt.co.uk/premier-league/startseite/wettbewerb/GB1
         @returns requests
@@ -17,13 +16,12 @@ class ClubsSpider(BaseSpider):
         @scrapes type href parent
         """
         def is_teams_table(table):
-            # Get all header texts from the table.
+            # Check the table headers; if any header contains the word "club" (case insensitive), we consider it a teams table.
             headers = [h.strip().lower() for h in table.css('th::text').getall() if h]
-            # We consider it a teams table if any header contains "club"
             return any("club" in hdr for hdr in headers)
 
         def extract_team_href(row):
-            # Expect that the second <td> contains the club link.
+            # We assume that the second <td> contains the team link.
             tds = row.css('td')
             if len(tds) >= 2:
                 return tds[1].css('a::attr(href)').get()
@@ -33,22 +31,26 @@ class ClubsSpider(BaseSpider):
         teams_tables = [table for table in page_tables if is_teams_table(table)]
         self.logger.info("Found %d responsive-table(s) on %s", len(teams_tables), response.url)
 
-        # Instead of asserting exactly one table, iterate over all found tables.
+        # Loop over each teams table (if more than one exists)
         for table in teams_tables:
             for row in table.css('tbody tr'):
                 href = extract_team_href(row)
                 if not href:
                     continue
-                # Remove any trailing season info (e.g. '/saison_id/2024')
-                href_strip_season = re.sub(r'/saison_id/\d{4}$', '', href)
+
+                # DO NOT strip out the season id. Use the link as provided (e.g. it includes /saison_id/2024)
+                # This preserves the correct season.
+                club_href = href  
+
                 cb_kwargs = {
                     'base': {
                         'type': 'club',
-                        'href': href_strip_season,
-                        'parent': parent  # parent comes from the competition item
+                        'href': club_href,
+                        'parent': parent  # parent is the competition item (which should already have correct competition_code, etc.)
                     }
                 }
-                yield response.follow(href, self.parse_details, cb_kwargs=cb_kwargs)
+
+                yield response.follow(club_href, self.parse_details, cb_kwargs=cb_kwargs)
 
     def parse_details(self, response, base):
         """
@@ -64,23 +66,31 @@ class ClubsSpider(BaseSpider):
         # Extract market value from the "dataMarktwert" section
         attributes['total_market_value'] = response.css('div.dataMarktwert a::text').get()
 
-        # Extract squad size and average age from the "dataContent" section
-        attributes['squad_size'] = self.safe_strip(response.xpath("//li[contains(text(),'Squad size:')]/span/text()").get())
-        attributes['average_age'] = self.safe_strip(response.xpath("//li[contains(text(),'Average age:')]/span/text()").get())
-
+        # Extract "Squad size" and "Average age" from the data content section
+        attributes['squad_size'] = self.safe_strip(
+            response.xpath("//li[contains(text(),'Squad size:')]/span/text()").get()
+        )
+        attributes['average_age'] = self.safe_strip(
+            response.xpath("//li[contains(text(),'Average age:')]/span/text()").get()
+        )
+        
         # Extract foreigners information
         foreigners_li = response.xpath("//li[contains(text(),'Foreigners:')]")
         if foreigners_li:
             attributes['foreigners_number'] = self.safe_strip(foreigners_li[0].xpath("span/a/text()").get())
-            attributes['foreigners_percentage'] = self.safe_strip(foreigners_li[0].xpath("span/span/text()").get())
+            attributes['foreigners_percentage'] = self.safe_strip(
+                foreigners_li[0].xpath("span/span/text()").get()
+            )
         else:
             attributes['foreigners_number'] = None
             attributes['foreigners_percentage'] = None
 
-        # Extract national team players
-        attributes['national_team_players'] = self.safe_strip(response.xpath("//li[contains(text(),'National team players:')]/span/a/text()").get())
+        # Extract national team players count
+        attributes['national_team_players'] = self.safe_strip(
+            response.xpath("//li[contains(text(),'National team players:')]/span/a/text()").get()
+        )
 
-        # Extract stadium details
+        # Extract stadium name and seating capacity
         stadium_li = response.xpath("//li[contains(text(),'Stadium:')]")
         if stadium_li:
             attributes['stadium_name'] = self.safe_strip(stadium_li[0].xpath("span/a/text()").get())
@@ -90,13 +100,16 @@ class ClubsSpider(BaseSpider):
             attributes['stadium_seats'] = None
 
         # Extract net transfer record and coach name
-        attributes['net_transfer_record'] = self.safe_strip(response.xpath("//li[contains(text(),'Current transfer record:')]/span/span/a/text()").get())
+        attributes['net_transfer_record'] = self.safe_strip(
+            response.xpath("//li[contains(text(),'Current transfer record:')]/span/span/a/text()").get()
+        )
         coach_name = response.xpath('//div[contains(@data-viewport, "Mitarbeiter")]//div[@class="container-hauptinfo"]/a/text()').get()
         attributes['coach_name'] = coach_name.strip() if coach_name else None
 
         # Extract a short code from the club URL and the club name
         attributes['code'] = unquote(urlparse(base["href"]).path.split("/")[1])
-        name_val = response.xpath("//span[@itemprop='legalName']/text()").get() or response.xpath('//h1[contains(@class,"data-header__headline-wrapper")]/text()').get()
+        name_val = response.xpath("//span[@itemprop='legalName']/text()").get() or \
+                   response.xpath('//h1[contains(@class,"data-header__headline-wrapper")]/text()').get()
         attributes['name'] = self.safe_strip(name_val)
 
         for key, value in attributes.items():
--- tfmkt/spiders/common.py
+++ tfmkt/spiders/common.py
@@ -1,8 +1,7 @@
 from io import BufferedReader
 import scrapy
 from scrapy import Request
-from scrapy.shell import inspect_response # required for debugging
-
+from scrapy.shell import inspect_response  # required for debugging
 import os, sys
 import json
 import gzip
@@ -11,111 +10,101 @@ import typing
 default_base_url = 'https://www.transfermarkt.co.uk'
 
 def read_lines(file_name: str, reading_fn: typing.Callable[[str], BufferedReader]) -> typing.List[dict]:
-  """A function that reads JSON lines from a file.
-
-  :param file_name: The name of the file to read from.
-  :type file_name: str
-  :param reading_fn: A function object to be used for opening the file.
-  :type reading_fn: typing.Callable[[str], BufferedReader]
-  :return: A list of json objects (dict)
-  :rtype: typing.List[dict]
-  """
-  with reading_fn(file_name) as f:
-    lines = f.readlines()
-    parents = [ json.loads(line) for line in lines ]
-  
-  return parents
+    """Reads JSON lines from a file."""
+    with reading_fn(file_name) as f:
+        lines = f.readlines()
+        parents = [json.loads(line) for line in lines]
+    return parents
 
 class BaseSpider(scrapy.Spider):
-  def __init__(self, base_url=None, parents=None, season=None):
+    def __init__(self, base_url=None, parents=None, season=None):
+        if base_url is not None:
+            self.base_url = base_url
+        else:
+            self.base_url = default_base_url
 
-    if base_url is not None:
-      self.base_url = base_url
-    else:
-      self.base_url = default_base_url
+        # Determine if the parents file is gzip compressed.
+        if parents is not None:
+            extension = parents.split(".")[-1]
+            if extension:
+                self.gzip_compressed = (extension == "gz")
+            else:
+                self.gzip_compressed = False
+        else:
+            self.gzip_compressed = False
 
-    # identify parents file extension (if any)
-    if parents is not None:
-      extension = parents.split(".")[-1]
-      if extension:
-        self.gzip_compressed = extension == "gz"
-      else: # if no extension, assume the file is not compressed
-        self.gzip_compressed = False
-    else:
-      self.gzip_compressed = False
-    
-    # load parent objects, either from stdin, a file or a zipped file
-    if parents is not None:
-      if self.gzip_compressed:
-        parents = read_lines(parents, gzip.open)
-      else:
-        parents = read_lines(parents, open)
-    elif not sys.stdin.isatty():
-        parents = [ json.loads(line) for line in sys.stdin ]
-    else:
-      parents = self.scrape_parents()
+        # Load parent objects from a file (or stdin)
+        if parents is not None:
+            if self.gzip_compressed:
+                parents = read_lines(parents, gzip.open)
+            else:
+                parents = read_lines(parents, open)
+        elif not sys.stdin.isatty():
+            parents = [json.loads(line) for line in sys.stdin]
+        else:
+            parents = self.scrape_parents()
 
-    # 2nd level parents are redundat
-    for parent in parents:
-      if parent.get('parent') is not None:
-        del parent['parent']
+        # Remove any second-level parent keys if present.
+        for parent in parents:
+            if parent.get('parent') is not None:
+                del parent['parent']
 
-    if season:
-      self.season = season
-    else:
-      self.season = 2022
+        # Set season. (Default is now 2025 instead of 2022.)
+        if season:
+            self.season = season
+        else:
+            self.season = 2025
 
-    self.entrypoints = parents
+        self.entrypoints = parents
 
-  def scrape_parents(self):
-    if not os.environ.get('SCRAPY_CHECK'):
-      raise Exception("Backfilling is not yet supported, please provide a 'parents' file")
-    else:
-      return []
+    def scrape_parents(self):
+        if not os.environ.get('SCRAPY_CHECK'):
+            raise Exception("Backfilling is not yet supported, please provide a 'parents' file")
+        else:
+            return []
 
-  def start_requests(self):
+    def start_requests(self):
+        """
+        Generate requests from entrypoints. Notice that we no longer filter out
+        clubs that aren’t marked as 'first_tier'—all parent items are used.
+        """
+        applicable_items = []
+        for item in self.entrypoints:
+            item['seasoned_href'] = self.seasonize_entrypoin_href(item)
+            applicable_items.append(item)
+        return [
+            Request(item['seasoned_href'], cb_kwargs={'parent': item})
+            for item in applicable_items
+        ]
 
-    applicable_items = []
+    def seasonize_entrypoin_href(self, item):
+        """
+        Construct a seasonized URL for the entrypoint.
+        
+        - For clubs: append /saison_id/<season>
+        - For competitions: if first_tier add /plus/0?saison_id=<season>,
+          if domestic cups, replace wettbewerb with pokalwettbewerb,
+          else just add ?saison_id=<season>.
+        - Otherwise: just append the href.
+        """
+        season = self.season
 
-    for item in self.entrypoints:
-      # clubs extraction is best done on first_tier competition types only
-      if self.name == 'clubs' and item['competition_type'] != 'first_tier':
-        continue
-      item['seasoned_href'] = self.seasonize_entrypoin_href(item)
-      applicable_items.append(item)
+        if item['type'] == 'club':
+            seasonized_href = f"{self.base_url}{item['href']}/saison_id/{season}"
+        elif item['type'] == 'competition':
+            if item.get('competition_type') == 'first_tier':
+                seasonized_href = f"{self.base_url}{item['href']}/plus/0?saison_id={season}"
+            elif item.get('competition_type') in ['domestic_cup', 'domestic_super_cup']:
+                seasonized_href = f"{self.base_url}{item['href']}?saison_id={season}".replace("wettbewerb", "pokalwettbewerb")
+            else:
+                seasonized_href = f"{self.base_url}{item['href']}?saison_id={season}"
+        else:
+            seasonized_href = f"{self.base_url}{item['href']}"
 
+        return seasonized_href
 
-    return [
-      Request(
-        item['seasoned_href'],
-        cb_kwargs={
-          'parent': item
-        }
-      )
-      for item in applicable_items
-    ]
-
-  def seasonize_entrypoin_href(self, item):
-
-    season = self.season
-
-    if item['type'] == 'club':
-      seasonized_href = f"{self.base_url}{item['href']}/saison_id/{season}"
-    elif item['type'] == 'competition':
-      if item['competition_type'] == 'first_tier':
-        seasonized_href = f"{self.base_url}{item['href']}/plus/0?saison_id={season}"
-      elif item['competition_type'] in ['domestic_cup', 'domestic_super_cup']:
-        seasonized_href = f"{self.base_url}{item['href']}?saison_id={season}".replace("wettbewerb", "pokalwettbewerb")
-      else:
-        seasonized_href = f"{self.base_url}{item['href']}?saison_id={season}"
-    else:
-      seasonized_href = f"{self.base_url}{item['href']}"
-
-    return seasonized_href
-
-  def safe_strip(self, word):
-    if word:
-      return word.strip()
-    else:
-      return word
-
+    def safe_strip(self, word):
+        if word:
+            return word.strip()
+        else:
+            return word
